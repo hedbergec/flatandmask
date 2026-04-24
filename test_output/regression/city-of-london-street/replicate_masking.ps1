9eb7d32c580d9f1ae533d27ff66aaf2fae7895d5
Eric C Hedberg <hedbergec@gmail.com>
2026-04-24 12:21:03 -0700

    testing updates

---
+++ test_output/regression/city-of-london-street/replicate_masking.ps1
@@ -0,0 +1,7 @@
+﻿param(
+    [string]$SecretKey = "testkey123",
+    [string]$OutputRoot = "$PSScriptRoot",
+    [int]$MaxCsvRows = 250
+)
+
+& "$PSScriptRoot\..\..\..\testing scripts\test-masking-tool.ps1" -SecretKey $SecretKey -OutputRoot $OutputRoot -MaxCsvRows $MaxCsvRows
